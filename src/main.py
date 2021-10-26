e292ef5324fec37ac0e54999aa0c005f6e415bb6
Johannes Mols <johannes.mols@googlemail.com>
2021-10-26 22:32:01 +0200

    Sending new events to miner instead of database

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import yaml
 import typing
 import logging
 import os
+import time
 import sender
 
 config: dict = yaml.safe_load(open('../config.yaml'))
@@ -20,9 +21,9 @@ def on_connect(_client: Client, userdata, flags, rc) -> None:
 def on_message(_client: Client, userdata, msg: MQTTMessage) -> None:
     hierarchy: typing.List[str] = msg.topic.split('/')
     if len(hierarchy) == 4:
-        event = MqttEvent(base=hierarchy[0], source=hierarchy[1], process=hierarchy[2],
-                          activity=hierarchy[3], payload=msg.payload.decode())
-        logging.info(f'Sending observed event to database: "{event}"')
+        event = MqttEvent(timestamp=int(time.time()), base=hierarchy[0], source=hierarchy[1],
+                          process=hierarchy[2], activity=hierarchy[3], payload=msg.payload.decode())
+        logging.info(f'Notifying miner with observed event: "{event}"')
         event_queue.put(event, block=True, timeout=10)
     else:
         logging.warning(f'Ignoring event with non-matching topic structure: {msg.topic}')
@@ -53,7 +54,7 @@ def setup_mqtt_client() -> Client:
 
 
 def setup_event_sender() -> Process:
-    sender_process = Process(target=sender.start, args=(event_queue, config['db']['address']))
+    sender_process = Process(target=sender.start, args=(event_queue, config['miner']['address']))
     sender_process.start()
     return sender_process
 
--- src/sender.py
+++ src/sender.py
@@ -9,8 +9,8 @@ def start(queue: Queue, address: str):
     while True:
         if not queue.empty():
             event: MqttEvent = queue.get(block=True, timeout=10)
-            result = httpx.post(address + '/events/add', json=event.to_dict())
-            logging.info(f'Posted event to DB with result: {result}')
+            result = httpx.post(address + '/notify', json=event.to_dict())
+            logging.info(f'Notified miner of new event with result: {result}')
         else:
             sleep(0.01)
 
